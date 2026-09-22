a9ef76bb9ed832f1e84b432798ac3dbb807aadcc
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-22 20:19:41 +0300

    feat: create HeroSection component with initial structure

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function HeroSection() {
+  return (
+    <section className="bg-[#F8FAFC] min-h-screen flex items-center">
+     
+    </section>
+  );
+}
+
+export default HeroSection;
