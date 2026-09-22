ad2fca070c72aaf7dc93249b5aa981178f756a95
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-22 22:31:54 +0300

    feat: refactor AuthContext to include login and logout methods

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,18 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
+export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+  };
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        setUser,
+        login,
+        logout,
       }}
     >
       {children}
     </AuthContext.Provider>
   );
-};
+}
+
+export const useAuth = () => useContext(AuthContext);
