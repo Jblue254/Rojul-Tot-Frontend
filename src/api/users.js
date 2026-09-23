48e00efadb4504c93862ff948db8493b73a8d507
Jblue254 <japhethkiprono2020@gmail.com>
2026-09-23 12:11:21 +0300

    feat: add deleteUser and updateUser API functions for user management

--- src/api/users.js
+++ src/api/users.js
@@ -3,3 +3,11 @@ import api from "./axios";
 export const getUsers = () => {
   return api.get("/auth/users/");
 };
+
+export const deleteUser = (id) => {
+  return api.delete(`/auth/users/${id}/`);
+};
+
+export const updateUser = (id, data) => {
+  return api.patch(`/auth/users/${id}/`, data);
+};
